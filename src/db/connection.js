3fc3042e5b767ef94d6268e374f19cdaff142d08
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-08-24 14:19:12 +0100

    fixed connection db

--- src/db/connection.js
+++ src/db/connection.js
@@ -9,15 +9,6 @@ import pg from "pg";
 // for connection information
 const { Pool } = pg;
 
-console.log(process.env.NODE_ENV);
-const db = new Pool({
-	ssl: {
-		rejectUnauthorized: false,
-	},
-	connectionString:
-		process.env.NODE_ENV !== "development"
-			? process.env.DATABASE_URL
-			: process.env.DATABASE_URL_DEV,
-});
+const db = new Pool();
 
 export default db;
--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const { PORT } = process.env;
 
 app.listen(PORT, async () => {
 	await createDefaultTables();
-	console.log(`Server is running on port  ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
 
 app.on("error", (error) => console.log(`Server is failed  :  ${error}`));
